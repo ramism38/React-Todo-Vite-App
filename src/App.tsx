f45af91bd430619b3988c051f5608a019d96e3d6
ramism38 <ismael.lg2002@gmail.com>
2026-03-24 19:14:43 +0100

    Lista de tareas inicial con estado + Boton para añadir tareas controlado + Actualizacion de la lista en tiempo real

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,48 @@ import './App.css'
 
 function App() {
 
+  //Tipo de tarea (Declaramos como sera el objeto)
   type Task = {
     id: number,
     text: string,
     completed: boolean;
   }
 
+  //Declaracion del array de tareas
   const [tasks, setTasks] = useState<Task[]>([
     { id: 1, text: "Aprender React", completed: false },
     { id: 2, text: "Hacer TODO App", completed: true }
   ]);
+
+  //Input controlado
+  const [taskName, setTaskName] = useState("");
+
+  //Actualizar input cuando el usuario escribe
+  const updateTaskName = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTaskName(event.target.value);
+  }
+
+  //Crear nueva tarea
+  const createTask = () => {
+    if (taskName.trim() === "") return; //Evitamos tareas vacias
+
+    //Generar nuevo ID con condiciones
+    let newId = 0;
+    if (tasks.length != 0) {
+      newId = tasks[tasks.length - 1].id + 1;
+    }
+
+    const newTask = {
+      id: newId,
+      text: taskName,
+      completed: false,
+    }
+
+    //Actualizar array y limpiar input
+    setTasks(prev => [...prev, newTask]);
+    setTaskName("")
+
+  }
 
   return (
     <div>
@@ -20,6 +52,9 @@ function App() {
         <li key={task.id}>Tarea nº {task.id} con nombre: {task.text} en estado: {task.completed ? "Completada" : "Pendiente"} </li>
       ))}
       </ul>
+      <br />
+      <input type="text" name="text" onChange={updateTaskName} value={taskName} />
+      <button type="button" onClick={createTask}>Añadir tarea</button>
     </div>
   )
 
